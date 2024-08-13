3da05488fb78a1f1883ba51b50c87f958c888385
Naru-hub <1208ny0324@gmail.com>
2024-08-13 21:40:52 +0900

    Welcomテンプレートのルート・レイアウト変更に伴う差分、ボタンのレイアウトも変更

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -1,11 +1,9 @@
 import { PropsWithChildren, ReactNode, useState } from "react";
 
-import ApplicationLogo from "@/Components/ApplicationLogo";
 import Dropdown from "@/Components/Dropdown";
 import NavLink from "@/Components/NavLink";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink";
 import { User } from "@/types";
-import { Link } from "@inertiajs/react";
 
 export default function Authenticated({
     user,
@@ -22,9 +20,12 @@ export default function Authenticated({
                     <div className="flex justify-between h-16">
                         <div className="flex">
                             <div className="shrink-0 flex items-center">
-                                <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
-                                </Link>
+                                <NavLink
+                                    href="/"
+                                    active={route().current("welcome")}
+                                >
+                                    Welcome
+                                </NavLink>
                             </div>
 
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
