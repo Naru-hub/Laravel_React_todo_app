cbda793e9aa9a572daacfe544b78b0b77e8a94c0
Naru-hub <1208ny0324@gmail.com>
2024-09-16 21:09:50 +0900

    バリデーションメッセージの日本語化ができていなかったところを修正

--- resources/lang/ja/validation.php
+++ resources/lang/ja/validation.php
@@ -7,7 +7,10 @@ return [
     'string' => ':attribute は最低 :min 文字必要です。',
   ],
   'confirmed' => ':attribute が一致しません。',
+  'email' => '正しいメールアドレス形式で入力してください。',
   'attributes' => [
     'password' => 'パスワード',
+    'name' => '名前',
+    'email' => 'メールアドレス',
   ],
 ];
