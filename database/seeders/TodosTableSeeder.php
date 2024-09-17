40e5b7e61fa184d96ddd33589f0b6df5d97c1c72
Nona_cal <1208ny0324@gmail.com>
2024-09-17 11:35:35 +0900

    todosテーブルに開始日と期限日のカラムを追加

--- database/seeders/TodosTableSeeder.php
+++ database/seeders/TodosTableSeeder.php
@@ -18,6 +18,8 @@ class TodosTableSeeder extends Seeder
             'title' => 'ジムに行く',
             'description' => 'ジムで運動してストレスと運動不足を解消する',
             'is_completed' => false,
+            'start_date' => now()->startOfDay(),
+            'due_date' => now()->addDays(2)->endOfDay(),
             'created_at' => now(),
             'updated_at' => now()
         ]);
@@ -27,6 +29,8 @@ class TodosTableSeeder extends Seeder
             'title' => '本を読む',
             'description' => '',
             'is_completed' => false,
+            'start_date' => now()->startOfDay(),
+            'due_date' => now()->addDays(3)->endOfDay(),
             'created_at' => now(),
             'updated_at' => now()
         ]);
@@ -36,6 +40,8 @@ class TodosTableSeeder extends Seeder
             'title' => 'プログラミングの勉強をする',
             'description' => 'PHPとJavaScriptの勉強をする',
             'is_completed' => false,
+            'start_date' => now()->copy()->addDays(2)->startOfDay(), // 作成日から2日後
+            'due_date' => now()->copy()->addDays(2)->addDays(3)->endOfDay(), // 開始日から3日後
             'created_at' => now(),
             'updated_at' => now()
         ]);
@@ -45,6 +51,8 @@ class TodosTableSeeder extends Seeder
             'title' => '犬と散歩に行く',
             'description' => 'トリミング前に散歩に連れて行く',
             'is_completed' => false,
+            'start_date' => now()->copy()->addDays(1)->startOfDay(), // 作成日から1日後
+            'due_date' => now()->copy()->addDays(1)->addDays(6)->endOfDay(), // 開始日から6日後
             'created_at' => now(),
             'updated_at' => now()
         ]);
@@ -54,6 +62,8 @@ class TodosTableSeeder extends Seeder
             'title' => '友達に電話する',
             'description' => '次の予定の時間を決める',
             'is_completed' => false,
+            'start_date' => now()->startOfDay(),
+            'due_date' => now()->addDays(5)->endOfDay(),
             'created_at' => now(),
             'updated_at' => now()
         ]);
