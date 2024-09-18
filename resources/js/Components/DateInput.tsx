8a8e2c712f90a51c0f200087015964aea3f1a9e2
Naru-hub <1208ny0324@gmail.com>
2024-09-18 22:14:21 +0900

    DatePickerのカレンダーを日本語表記に変更

--- resources/js/Components/DateInput.tsx
+++ resources/js/Components/DateInput.tsx
@@ -1,26 +1,30 @@
-import React, { forwardRef, InputHTMLAttributes, useRef, useImperativeHandle, useEffect } from 'react';
-import 'react-datepicker/dist/react-datepicker.css';
-import { DateInputProps } from '@/types';
+import "react-datepicker/dist/react-datepicker.css";
 
-import DatePicker from 'react-datepicker';
+import { ja } from "date-fns/locale";
+import React, { forwardRef } from "react";
+import DatePicker, { registerLocale } from "react-datepicker";
+
+import { DateInputProps } from "@/types";
 
 const DateInput = forwardRef<DatePicker, DateInputProps>(
-    ({ selected, onChange, className}, ref) => {
+    ({ selected, onChange, className }, ref) => {
         const Today = new Date();
+        registerLocale("ja", ja);
 
         return (
             <DatePicker
-            selected={selected ?? undefined} // `null` を `undefined` に変換
-            onChange={date => onChange(date)}
-            ref={ref as React.RefObject<DatePicker>} // RefObject<DatePicker> にキャスト
-            dateFormat="yyyy/MM/dd"
-            minDate={Today}
-            className={`border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm block w-full ${className}`}
-        />
+                selected={selected ?? undefined} // `null` を `undefined` に変換
+                onChange={(date) => onChange(date)}
+                ref={ref as React.RefObject<DatePicker>} // RefObject<DatePicker> にキャスト
+                dateFormat="yyyy/MM/dd"
+                locale="ja"
+                minDate={Today}
+                className={`border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm block w-full ${className}`}
+            />
         );
     }
 );
 
-DateInput.displayName = 'DateInput';
+DateInput.displayName = "DateInput";
 
 export default DateInput;
