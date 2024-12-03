f5322e2e109b27efc4c9f43e3f5457ff2c5ecbf6
Naru-hub <1208ny0324@gmail.com>
2024-12-03 21:39:51 +0900

    見た目整え

--- resources/js/Pages/Team/Todo/Detail.tsx
+++ resources/js/Pages/Team/Todo/Detail.tsx
@@ -142,7 +142,7 @@ export default function TeamTodoDetail({ auth, todo }: PageProps) {
                                             {todoData.team_id}
                                         </dd>
                                     </div>
-                                    <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                         <dt className="text-sm font-medium text-gray-500">
                                             チーム名
                                         </dt>
@@ -150,6 +150,7 @@ export default function TeamTodoDetail({ auth, todo }: PageProps) {
                                             {todoData.team?.name}
                                         </dd>
                                     </div>
+                                    <div className="border-t border-gray-100"></div>
                                     <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                         <dt className="text-sm font-medium text-gray-500"></dt>
                                         <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
