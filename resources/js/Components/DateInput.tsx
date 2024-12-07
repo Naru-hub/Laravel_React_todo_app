091629fd506bfe483a84d5c81ffbeff16ad99afe
Naru-hub <1208ny0324@gmail.com>
2024-12-07 13:00:48 +0900

    DateInputコンポーネントでidを受け取り、DatePickerに適用するように修正

--- resources/js/Components/DateInput.tsx
+++ resources/js/Components/DateInput.tsx
@@ -7,12 +7,13 @@ import DatePicker, { registerLocale } from "react-datepicker";
 import { DateInputProps } from "@/types";
 
 const DateInput = forwardRef<DatePicker, DateInputProps>(
-    ({ selected, onChange, className }, ref) => {
+    ({ id, selected, onChange, className }, ref) => {
         const Today = new Date();
         registerLocale("ja", ja);
 
         return (
             <DatePicker
+                id={id}
                 selected={selected ?? undefined} // `null` を `undefined` に変換
                 onChange={(date) => onChange(date)}
                 ref={ref as React.RefObject<DatePicker>} // RefObject<DatePicker> にキャスト
